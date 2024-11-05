1110784decec7386166f2b0c47af87442d81c77e
prakash-mty <jyoti.prakashmty@gmail.com>
2024-11-06 00:06:35 +0530

    added new classes

--- src/OopsConcepts/UpcastingAndDownCasting.java
+++ src/OopsConcepts/UpcastingAndDownCasting.java
@@ -4,11 +4,16 @@ class Parent {
     void show() {
         System.out.println("This is parent class method.");
     }
+    void display()
+    {
+        System.out.println("Inside display method");
+    }
 }
 
 class ChildNew extends Parent {
     //method overriding
     void show() {
+        super.show();
         System.out.println("This is child class method");
     }
 }
@@ -16,9 +21,12 @@ class ChildNew extends Parent {
 public class UpcastingAndDownCasting {
     public static void main(String[] args) {
         Parent p = new Parent();
-        p.show();//--->parent class method
-        p = new ChildNew();
-        p.show();//upcasting so type casting optional--> child class method
+       // p.show();//--->parent class method
+        p = new ChildNew();//upcasting
+
+        p.display();
+        p.show();//upcasting so type casting optional--> child class method--->overridden so to get parent class
+        //method we need to add super inside child class overridden method.
         ChildNew c = (ChildNew) p;//down casting
         c.show();
     }
